eadc86dd19cfb3349196b1b52b079819a79dc828
Bronstrom <bafellstrom@gmail.com>
2021-11-11 00:22:30 -0500

    Add blog 8 & update editor to render immediately

--- js/live-editor-app.js
+++ js/live-editor-app.js
@@ -1,21 +1,28 @@
+function renderDoc(html, css, js, render) {
+    render.open();
+    render.writeln(
+        html.value +
+        "<style>" +
+        css.value +
+        "</style>" +
+        "<script>" +
+        js.value +
+        "</script>"
+    );
+    render.close();
+};
+
 function compileAndRender() {
     var html = document.getElementById("html");
     var css = document.getElementById("css");
     var js = document.getElementById("js");
     var render = document.getElementById("render").contentWindow.document;
 
+    // Initial render on page load
+    renderDoc(html, css, js, render);
+    // Record render on key up event
     document.body.onkeyup = function () {
-        render.open();
-        render.writeln(
-            html.value +
-            "<style>" +
-            css.value +
-            "</style>" +
-            "<script>" +
-            js.value +
-            "</script>"
-        );
-        render.close();
+        renderDoc(html, css, js, render);
     };
 }
 
